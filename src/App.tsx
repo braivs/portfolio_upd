40d76620528e3d38bdca63b8cb50da0ff65222d8
Briws <brightwiths@gmail.com>
2023-10-04 13:53:48 +0400

    copied updated project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import s from './App.module.scss'
-import {Router} from "./router"
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {Portfolio} from "./components/Portfolio/Portfolio"
+import {CV} from "./components/CV/CV"
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Portfolio />,
+    },
+    // {
+    //   path: '/cv',
+    //   element: <CV />,
+    // },
+  ])
   return (
     <div className={s.app}>
-      <Router />
+      <RouterProvider router={router} />
     </div>
   );
 }
